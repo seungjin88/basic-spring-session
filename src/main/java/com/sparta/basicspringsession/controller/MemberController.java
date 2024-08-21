6b3eefd6e50c5f0c39310b3433b52b353914045c
seungjin88 <powerjinny@hanmail.net>
2024-08-21 23:20:03 +0900

    과제 제ì¶출

--- src/main/java/com/sparta/basicspringsession/controller/MemberController.java
+++ src/main/java/com/sparta/basicspringsession/controller/MemberController.java
@@ -1,13 +1,28 @@
 package com.sparta.basicspringsession.controller;
 
 
+import com.sparta.basicspringsession.entity.Member;
 import com.sparta.basicspringsession.service.MemberService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
 public class MemberController {
 
     private final MemberService memberService;
+
+    @PostMapping("/members")
+    public void saveMember() {
+        memberService.saveMember();
+    }
+
+    @GetMapping("/members")
+    public List<Member> getMembers() {
+        return memberService.getMembers();
+    }
 }
--- src/main/java/com/sparta/basicspringsession/service/MemberService.java
+++ src/main/java/com/sparta/basicspringsession/service/MemberService.java
@@ -1,12 +1,25 @@
 package com.sparta.basicspringsession.service;
 
+import com.sparta.basicspringsession.entity.Member;
 import com.sparta.basicspringsession.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class MemberService {
 
     private final MemberRepository memberRepository;
+
+    public void saveMember() {
+        Member member = new Member("새로운 맴버");
+        memberRepository.save(member);
+
+    }
+
+    public List<Member> getMembers() {
+        return memberRepository.findAll();
+    }
 }
