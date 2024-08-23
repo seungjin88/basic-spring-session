d81bf410638b813ac8842a3b80f452176cf1fe8a
seungjin88 <powerjinny@hanmail.net>
2024-08-23 13:54:40 +0900

    2차 제출

--- src/main/java/com/sparta/basicspringsession/controller/MemberController.java
+++ src/main/java/com/sparta/basicspringsession/controller/MemberController.java
@@ -1,12 +1,11 @@
 package com.sparta.basicspringsession.controller;
 
 
-import com.sparta.basicspringsession.entity.Member;
 import com.sparta.basicspringsession.service.MemberService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.sparta.basicspringsession.dto.*;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -17,12 +16,27 @@ public class MemberController {
     private final MemberService memberService;
 
     @PostMapping("/members")
-    public void saveMember() {
-        memberService.saveMember();
+    public ResponseEntity<MemberSaveResponseDto> saveMember(@RequestBody MemberSaveRequestDto requestDto) {
+        return ResponseEntity.ok(memberService.saveMember(requestDto));
     }
 
     @GetMapping("/members")
-    public List<Member> getMembers() {
-        return memberService.getMembers();
+    public ResponseEntity<List<MemberSimpleResponseDto>> getMembers() {
+        return ResponseEntity.ok(memberService.getMembers());
+    }
+
+    @GetMapping("/members/{memberId")
+    public ResponseEntity<MemberDetailResponseDto> getMember(@PathVariable Long memberId) {
+        return ResponseEntity.ok(memberService.getMember(memberId));
+    }
+
+    @PutMapping("/members/{memberId}")
+    public ResponseEntity<MemberUpdateResponseDto> updateMember(@PathVariable Long memberId, @RequestBody MemberUpdateRequestDto requestDto) {
+        return ResponseEntity.ok(memberService.updateMember(memberId, requestDto));
+    }
+
+    @DeleteMapping("/members/{memberId}")
+    public void deleteMember(@PathVariable Long memberId) {
+        memberService.deleteMember(memberId);
     }
 }
--- src/main/java/com/sparta/basicspringsession/service/MemberService.java
+++ src/main/java/com/sparta/basicspringsession/service/MemberService.java
@@ -1,10 +1,13 @@
 package com.sparta.basicspringsession.service;
 
+import com.sparta.basicspringsession.dto.*;
 import com.sparta.basicspringsession.entity.Member;
 import com.sparta.basicspringsession.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -13,13 +16,45 @@ public class MemberService {
 
     private final MemberRepository memberRepository;
 
-    public void saveMember() {
-        Member member = new Member("새로운 맴버");
-        memberRepository.save(member);
+    @Transactional
+    public MemberSaveResponseDto saveMember(MemberSaveRequestDto requestDto) {
+        Member newMember = new Member(requestDto.getName());
+        Member savedMember = memberRepository.save(newMember);
 
+        return new MemberSaveResponseDto(savedMember.getId(), savedMember.getName());
     }
 
-    public List<Member> getMembers() {
-        return memberRepository.findAll();
+    public List<MemberSimpleResponseDto> getMembers() {
+        List<Member> memberList = memberRepository.findAll();
+
+        List<MemberSimpleResponseDto> dtoList = new ArrayList<>();
+        for (Member member : memberList) {
+            MemberSimpleResponseDto dto = new MemberSimpleResponseDto(member.getName());
+            dtoList.add(dto);
+        }
+
+        return dtoList;
+    }
+
+    public MemberDetailResponseDto getMember(Long memberId) {
+        Member member = memberRepository.findById(memberId).orElseThrow(() -> new NullPointerException("멤버가 없습니다."));
+
+        return new MemberDetailResponseDto(member.getId(), member.getName());
+    }
+
+    @Transactional
+    public MemberUpdateResponseDto updateMember(Long memberId, MemberUpdateRequestDto requestDto) {
+        Member member = memberRepository.findById(memberId).orElseThrow(() -> new NullPointerException("멤버가 없습니다."));
+
+        member.update(requestDto.getName());
+
+        return new MemberUpdateResponseDto(member.getId(), member.getName());
+    }
+
+    @Transactional
+    public void deleteMember(Long memberId) {
+        Member member = memberRepository.findById(memberId).orElseThrow(() -> new NullPointerException("멤버가 없습니다."));
+
+        memberRepository.delete(member);
     }
 }
